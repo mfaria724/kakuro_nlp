415b6941f4320adadca789585453fb0b645f9d9c
Amin Arriaga <16-10072@usb.ve>
2021-04-10 16:40:08 -0400

    El script funciona para kakuros pequennitos.

--- kakuro.pl
+++ kakuro.pl
@@ -39,9 +39,6 @@ verifyClues([clue(X, Y, S, L)|ClueList]) :-
   clue(X, Y, S, L),
   verifyClues(ClueList).
 
-
-% VERIFICACION DE QUE UNA CASILLA CONTIENE A LO SUMO 2 CLUES.
-
 % maxClues(C, L, N) verifica que en L hay a lo sumo N clues con la misma
 % casilla que el clue C.
 maxClues(_, [], N) :- N >= 0.
@@ -121,15 +118,8 @@ kakuro(Clues) :-
 
 
 
+
 % ================== CORRECTITUD DE UNA SOLUCION ================== %
-fill(blank(_, _), N) :- 0 < N, N < 10.
-
-% verifyFills(F) verifica que F es una lista de fills valida.
-verifyFills([]).
-verifyFills([fill(blank(Fx, Fy), N)|FillList]) :-
-  fill(blank(Fx, Fy), N),
-  verifyFills(FillList).
-
 % notInFIlls(X, Y, F) verifica que el punto (X, Y) no esta en ningun
 % fill de la lista de fills F.
 notInAnyFill(_, _, []).
@@ -144,17 +134,28 @@ fillNotInAnyFill([fill(blank(X, Y), _)|FillList]) :-
   notInAnyFill(X, Y, FillList),
   fillNotInAnyFill(FillList).
 
+% Toma solo el primer fill que se encuentre en el mismo lugar,
+takeFirstFill(fill(blank(X, Y), N), [fill(blank(X, Y), M)|_]) :- !, N = M.
+takeFirstFill(fill(blank(X, Y), N), [fill(blank(P, Q), _)|FillList]) :- 
+  (X \= P ; Y \= Q),
+  takeFirstFill(fill(blank(X, Y), N), FillList).
+
 % getFills(B, A, F) es tal que F es la lista de fills respecto a B que
 % se encuentran en A.
 getFills([], _, []).
 getFills([blank(X, Y)|BlankList], AllFills, [fill(blank(X, Y), N)|FillList]) :-
-  member(fill(blank(X, Y), N), AllFills),
+  takeFirstFill(fill(blank(X, Y), N), AllFills),
   getFills(BlankList, AllFills, FillList).
+
+% Verificamos si un numero N es tal que 0 < N < 10.
+isDigit(X) :- member(X, [9,8,7,6,5,4,3,2,1]).
 
 % notSameNumber(F, L) verifica que el numero del fill F no se encuentra en ningun
 % fill de la lista L.
 notSameNumber(_, []).
 notSameNumber(fill(X, N), [fill(_, M)|FillList]) :-
+  isDigit(N),
+  isDigit(M),
   N \= M,
   notSameNumber(fill(X, N), FillList).
 
@@ -164,31 +165,81 @@ allNumbersDifferents([Fill|FillList]) :-
   notSameNumber(Fill, FillList),
   allNumbersDifferents(FillList).
 
-% getSum(F, S) es tal que S es la suma de los numero de los fills de F.
-getSum([], 0).
-getSum([fill(_, N)|FillList], Sum) :-
-  M is Sum - N,
-  getSum(FillList, M).
-
 % correctClue(C, F) es tal que F son todos los fills y clue cumple con que sus fills
-% correspondientes suman el numero del clue y todos tienen numeros distintos.
-correctClue(clue(_, _, Sum, BlankList), AllFills) :-
-  getFills(BlankList, AllFills, FillList),
-  allNumbersDifferents(FillList),
-  getSum(FillList, Sum).
+% correspondientes todos tienen numeros distintos.
+correctClue(clue(_, _, _, BlankList), AllFills) :-
+  getFills(BlankList, AllFills, FillList), !,
+  allNumbersDifferents(FillList).
 
 % correctAllClues(C, F) es tal que F son todos los fills y todos los clues de C son
-% correctos.
+% tal que los fills correspondientes tienen numeros distintos.
 correctAllClues([], _).
 correctAllClues([Clue|ClueList], AllFills) :-
   correctClue(Clue, AllFills),
   correctAllClues(ClueList, AllFills).
 
+% getSum(F, S) es tal que S es la suma de los numero de los fills de F.
+getSum([], 0).
+getSum([fill(_, N)|FillList], Sum) :-
+  Sum > 0,
+  M is Sum - N,
+  getSum(FillList, M).
+
+% correctClue(C, F) es tal que F son todos los fills y clue cumple con que sus fills
+% correspondientes suman el valor que deben.
+correctSumClue(clue(_, _, Sum, BlankList), AllFills) :-
+  getFills(BlankList, AllFills, FillList),
+  getSum(FillList, Sum).
+
+% correctAllClues(C, F) es tal que F son todos los fills y todos los clues de C son
+% tal que los fills correspondientes suman el valor que deben.
+correctAllSumClues([], _).
+correctAllSumClues([Clue|ClueList], AllFills) :-
+  correctSumClue(Clue, AllFills),
+  correctAllSumClues(ClueList, AllFills).
+
+% Verifica que todos los elementos de una lista son ground.
+allGrounds([]).
+allGrounds([X|L]) :- ground(X), allGrounds(L).
+
+% Verifica que un kakuro se cumple dada una solucion.
+isSolution(Kakuro, Solution) :-
+  correctAllClues(Kakuro, Solution),
+  correctAllSumClues(Kakuro, Solution),
+  allGrounds(Solution),
+  fillNotInAnyFill(Solution).
+
+% Verificamos si existe alguna otra solucion.
+otherSolution(Kakuro, Solution, OtherSolution) :-
+  isSolution(Kakuro, OtherSolution),
+  OtherSolution \= Solution.
+
+% Una solucion es valida si cumple el Kakuro y no existe otra solucion.
 valid(Kakuro, Solution) :-
   kakuro(Kakuro),
-  verifyFills(Solution),
-  correctAllClues(Kakuro, Solution).
+  isSolution(Kakuro, Solution), !,
+  not(otherSolution(Kakuro, Solution, _)).
 
+valid(Kakuro) :-
+  kakuro(Kakuro), nl,
+  ( 
+    % Si hay solucion
+    (isSolution(Kakuro, Solution), !) ->
+    % Ejecutamos esto
+    (
+      print('Se encontro la siguiente solucion: '), nl,
+      write(Solution), nl, nl,
+      (
+        not(otherSolution(Kakuro, Solution, _)) ->
+        ( print('Y es la unica solucion posible!'), nl, !) ;
+        (
+          print('Pero se consiguo otra solucion: '), nl
+        )
+      )
+    );
+    % Si no, error.
+    print('No existe una solucion valida.\n'), !
+  ).
 
 
 % ================== LECTURA DE UN KAKURO ================== %
